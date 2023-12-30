cefe580f222ede561827dfd799d8ff3e716da9af
Ali Baan <codewithaliburhan@gmail.com>
2023-12-30 20:15:06 +0500

    Second commit

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,11 @@
+import GitHubProvider from 'next-auth/providers/github'
+
+export const options = {
+    providers: [
+        GitHubProvider({
+            clientId:process.env.GITHUB_CLIENT_ID,
+            clientSecret:process.env.GITHUB_CLIENT_SECRET
+        })
+    ],
+    secret:process.env.NEXT_AUTH_SECRET
+}
